ed3997f39b76b7079522f7373229d1429a3dadf4
AlexManhes <92828095+AlexManhes@users.noreply.github.com>
2021-12-21 13:16:34 -0300

    APIcore

---
+++ APIcore/APIcore/Models/sos.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIcore.Models
+{
+    
+    public class sosEditar
+    {
+        [Key]
+        public int matricula { get; set; }
+        public string nome { get; set; }
+        
+    }
+}
